79e1e7b510baec0df62ace75c51b338a9c638285
asaadgaini <asaad.gini@gmail.com>
2024-09-16 13:50:22 +0100

    react and tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-2xl text-cyan-700
+    '>
+      app
+      <h1></h1>
+    </div>
+  )
+}
+
+export default App
+a
