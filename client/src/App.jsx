5421c60480897b08d360371ceb9a24bef3b4526b
asaadgaini <asaad.gini@gmail.com>
2024-09-16 16:09:28 +0100

    pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
-    <div className='text-2xl text-cyan-700
-    '>
-      app
-      <h1></h1>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
+)}
 
 export default App
-a
