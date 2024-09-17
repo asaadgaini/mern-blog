499c2a14ec7842743324649d8636f67a67a030fa
asaadgaini <asaad.gini@gmail.com>
2024-09-17 12:04:36 +0100

    connect with the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config()
+const app = express();
+mongoose
+  .connect(process.env.Mongo)
+  .then(() => {
+    console.log("db is connect");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-const app = express()
-
-app.listen(3000 , ()=>{
-    console.log("server is runing in 3000 port")
-})
+app.listen(3000, () => {
+  console.log("server is runing in 3000 port");
+});
