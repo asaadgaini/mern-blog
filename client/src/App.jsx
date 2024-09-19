93fa88ae98614e32fe50ac79f1f856537555cb89
asaadgaini <asaad.gini@gmail.com>
2024-09-19 08:32:48 +0100

    redux  stup in signin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,8 +8,9 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
 
+
 function App() {
-  return (
+   return (
     <BrowserRouter>
       <Header/>
       <Routes>
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { Link , useNavigate } from "react-router-dom";
+import {useDispatch, useSelector} from 'react-redux'
+import { signInStart , signInSuccess,signInFailure } from "../components/redux/user/userSlice";
 
 function SignUp() {
   const [formData , setFormData] = useState({})
-  const [error , setError] = useState(null)
-  const [loading  , setLoading] = useState(false)
+  const {loading , error }=useSelector((state)=>state.user)
   const navigate = useNavigate()
+  const dispatch= useDispatch()
   const handleChange = (e)=>{
     setFormData({
       ...formData , 
@@ -15,7 +17,8 @@ function SignUp() {
   }
   const handleSubmit  = async (e)=>{
     e.preventDefault()
-    setLoading(true)
+  try {
+    dispatch(signInStart())
     const res = await fetch("/api/auth/signin" ,{
       method : "POST",
       headers:{
@@ -25,15 +28,16 @@ function SignUp() {
     } )
     const data = await res.json()
     if(data.success=== false){
-      setError(data.message)
-      setLoading(false)
+      dispatch(signInFailure(data.message))
       return
     }
-    setLoading(false)
-    setError(null)
+    dispatch(signInSuccess(data))
     navigate("/")
     console.log(data)
-  }
+  
+  } catch (error) {
+    dispatch(signInFailure(error.message))
+  }}
   return (
     <div className="p-3 mx-auto max-w-lg">
       <h1 className="text-3xl font-semibold text-center my-7 ">Sign In</h1>
