37f674fbaaff678c570bd89eedddf99e1cad5046
asaadgaini <asaad.gini@gmail.com>
2024-09-19 09:15:03 +0100

    add redux persist

--- client/src/components/redux/store.js
+++ client/src/components/redux/store.js
@@ -1,13 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from './user/userSlice.js';
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+// Combine reducers
+const rootReducer = combineReducers({ user: userReducer });
+
+// Persistence configuration
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+// Create persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure store with the persisted reducer
 export const store = configureStore({
-  reducer: {
-    user: userReducer
-  },
-  middleware: (getDefaultMiddleware) => 
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+// Create the persisted store
+export const persistor = persistStore(store);
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,17 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { store } from './components/redux/store.js';
+import { persistor, store } from './components/redux/store.js';
 import { Provider } from 'react-redux'; // Corrected import for Provider
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <App />
+
+      </PersistGate>
     </Provider>
   </StrictMode>,
 );
